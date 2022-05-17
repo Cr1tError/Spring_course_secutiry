284993133ee049c7e6c826ea4360cd91422beec7
Cr1tError <maksym.kokora@gmail.com>
2022-05-17 15:45:27 +0200

    add authorize roles.

--- src/main/java/com/org/mk/security/configuration/MySecurityConfig.java
+++ src/main/java/com/org/mk/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.org.mk.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,6 +27,16 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
               .withUser(userBuilder.username("Oleg")
                       .password("user")
                       .roles("MANAGER", "HR"));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
 
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_emps.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_emps.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
@@ -7,10 +8,15 @@
 <body>
     <h3> Information for all Employees </h3>
     <br>
-    <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
-Only for HR staff.
+    <security:authorize access="hasRole('HR')"> <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
+        Only for HR staff.
+    </security:authorize>
+
 <br>
-    <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
-Only for managers.
+    <security:authorize access="hasRole('MANAGER')">
+        <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
+        Only for managers.
+    </security:authorize>
+
 </body>
 </html>
